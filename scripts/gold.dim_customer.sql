a18d09c309a4f058b134a34a0eb5deaa452727b8
Shreyaas Dhamore <shreyassureshdhamore1@gmail.com>
2026-02-14 11:05:53 +0530

    Create gold.dim_customer.sql

---
+++ scripts/gold.dim_customer.sql
@@ -0,0 +1,19 @@
+create view gold.dim_customer as 
+select 
+	row_number() over (order by cst_id ) as customer_key,
+	ci.cst_id as customer_id,
+	ci.cst_key as customer_number,
+	ci.cst_firstname as customer_firstname,
+	ci.cst_lastname as customer_lastname,
+	la.cntry,
+	ci.cst_marital_status as marital_status,
+	case when ci.cst_gndr != 'n/a' THEN ci.cst_gndr
+		 else coalesce(ca.gen,'n/a')
+	end as gender,
+	ci.cst_create_date as create_date,
+	ca.bdate as birthday	
+from silver.crm_cust_info ci
+left join silver.erp_cust_az12 ca 
+on ci.cst_key = ca.cid
+left join silver.erp_loc_a101 la
+on ci.cst_key = la.cid
